7c56fb1c0e6e95b8930b3442218b3a3c86a21019
Manrique <angie27manrique@gmail.com>
2019-10-18 21:20:26 -0500

    calendario y documentación

--- src/main/java/com/udec/registrocarrosjsf/controlador/PrincipalControl.java
+++ src/main/java/com/udec/registrocarrosjsf/controlador/PrincipalControl.java
@@ -15,92 +15,185 @@ import javax.enterprise.context.SessionScoped;
 
 import javax.inject.Named;
 
-
 /**
+ * Esta clase contiene los datos que van a ser guardados de los vehiculos
  *
- * @author ASUS
+ * @author Cristian Perez, Angie Manrique
  */
 @Named(value = "principalControl")
 @SessionScoped
-public class PrincipalControl implements Serializable{
+public class PrincipalControl implements Serializable {
+
+    /**
+     * variable que guarda el nombre del carro
+     */
     private String nombre;
+    /**
+     * variable que guarda la marca del carro
+     */
     private String marca;
+    /**
+     * variable que guarda el modelo del carro
+     */
     private int modelo;
+    /**
+     * lista de marcas de carros
+     */
     private List<String> marcas;
+    /**
+     * lista de los carros que se registran en el programa
+     */
     private List<Carro> listaCarros;
+    /**
+     * lista que guarda los registros de carros segun el filtro de busqueda
+     */
     private List<Carro> carrosfiltrados;
-    
+    /*
+     Método que llama al método encargado del listado de marcas
+     */
+
     @PostConstruct
     public void init() {
         llenarListaMarcas();
     }
+
+    /**
+     * Constructor de la clase, aquí se crea una lista de carros
+     */
     public PrincipalControl() {
-        listaCarros=new ArrayList();
+        listaCarros = new ArrayList();
     }
 
+    /**
+     * Método que obtiene la lista de marcas de carros
+     *
+     * @return
+     */
     public List<String> getMarcas() {
         return marcas;
     }
 
+    /**
+     * Método que le da valor a la lista de marcas
+     *
+     * @param marcas
+     */
     public void setMarcas(List<String> marcas) {
         this.marcas = marcas;
     }
 
+    /**
+     * obtiene el nombre del carro
+     *
+     * @return
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * le da valor al nombre del carro
+     *
+     * @param nombre
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Método que obtiene la marca del carro
+     *
+     * @return
+     */
     public String getMarca() {
         return marca;
     }
 
+    /**
+     * Método que le da valor a la marca del carro
+     *
+     * @param marca
+     */
     public void setMarca(String marca) {
         this.marca = marca;
     }
 
+    /**
+     * Método que obtiene el modelo del carro
+     *
+     * @return
+     */
     public int getModelo() {
         return modelo;
     }
 
+    /**
+     * Método que le da valor al modelo del carro
+     *
+     * @param modelo
+     */
     public void setModelo(int modelo) {
         this.modelo = modelo;
     }
 
+    /**
+     * Método que obtiene la lista de carros registrados
+     *
+     * @return
+     */
     public List<Carro> getListaCarros() {
         return listaCarros;
     }
 
+    /**
+     * Método que le da valor a los parametros de los carros registrados
+     *
+     * @param listaCarros
+     */
     public void setListaCarros(List<Carro> listaCarros) {
         this.listaCarros = listaCarros;
     }
 
+    /**
+     * Retorna los carros segun el criterio de buscada usado
+     *
+     * @return
+     */
     public List<Carro> getCarrosfiltrados() {
         return carrosfiltrados;
     }
 
+    /**
+     * Le da valor a la lista de carros segun la busqueda
+     *
+     * @param carrosfiltrados
+     */
     public void setCarrosfiltrados(List<Carro> carrosfiltrados) {
         this.carrosfiltrados = carrosfiltrados;
     }
-    
-    public void llenarListaMarcas(){
-        marcas= new ArrayList();
-            marcas.add("Renault");
-            marcas.add("Chevrolet");
-            marcas.add("Mercedes");
-            marcas.add("Audi");
-            marcas.add("Toyota");
-            marcas.add("Renault");
-            marcas.add("Ferrari"); 
-            marcas.add("Dodge"); 
+
+    /**
+     * Método que se encarga de agregar las marcas de carros a una lista
+     */
+    public void llenarListaMarcas() {
+        marcas = new ArrayList();
+        marcas.add("Renault");
+        marcas.add("Chevrolet");
+        marcas.add("Mercedes");
+        marcas.add("Audi");
+        marcas.add("Toyota");
+        marcas.add("Renault");
+        marcas.add("Ferrari");
+        marcas.add("Dodge");
     }
-    public void llenarListaCarros(){
-      Carro carro=new Carro(nombre, marca, modelo);
-      listaCarros.add(carro);
-        System.out.println(""+carro.getMarca());
+
+    /**
+     * Método que crea los carros y los guarda en una lista
+     */
+    public void llenarListaCarros() {
+        Carro carro = new Carro(nombre, marca, modelo);
+        listaCarros.add(carro);
+        System.out.println("" + carro.getMarca());
     }
-    
+
 }
--- src/main/java/com/udec/registrocarrosjsf/modelo/Carro.java
+++ src/main/java/com/udec/registrocarrosjsf/modelo/Carro.java
@@ -6,35 +6,75 @@
 package com.udec.registrocarrosjsf.modelo;
 
 /**
+ * Esta clase contiene los atributos de los carros
  *
- * @author ASUS
+ * @author Cristian Perez, Angie Manrique
  */
 public class Carro {
-    
+
+    /**
+     * Variable que guarda el nombre del carro
+     */
     private String nombre;
+    /**
+     * Variable que guarda la marca del carro
+     */
     private String marca;
+    /**
+     * Variable que guarda el modelo del carro
+     */
     private int modelo;
 
+    /**
+     * Método que obtiene el nombre del carro
+     *
+     * @return
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Método que le da valor al nombre del carro
+     *
+     * @param nombre
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Método que obtiene la marca del carro
+     *
+     * @return
+     */
     public String getMarca() {
         return marca;
     }
 
+    /**
+     * Método que le da valor a la marca del carro
+     *
+     * @param marca
+     */
     public void setMarca(String marca) {
         this.marca = marca;
     }
 
+    /**
+     * Método que obtiene el modelo del carro
+     *
+     * @return
+     */
     public int getModelo() {
         return modelo;
     }
 
+    /**
+     * Método que le da valor al modelo del carro
+     *
+     * @param modelo
+     */
     public void setModelo(int modelo) {
         this.modelo = modelo;
     }
@@ -44,5 +84,5 @@ public class Carro {
         this.marca = marca;
         this.modelo = modelo;
     }
-    
+
 }
